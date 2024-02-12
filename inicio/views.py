2a1204b7644afa9e423e71cb2a96a8028f86f49e
agustintapiabi <agustintapia.1111@gmail.com>
2024-02-12 00:32:00 -0300

    build.sh new

--- inicio/views.py
+++ inicio/views.py
@@ -27,7 +27,7 @@ def signup(request):
                 username=request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return redirect('inventario')
+                return redirect('indexsoftware')
             except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
